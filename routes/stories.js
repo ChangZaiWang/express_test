c34981d4176c267a08f05b6bd3c970bed6cb184d
ChangZaiWang <xs950179@gmail.com>
2021-07-12 16:26:57 +0800

    version 2

--- routes/stories.js
+++ routes/stories.js
@@ -13,13 +13,20 @@ router.get('/', async(req, res) => {
     const results = await db.promise().query(`SELECT * FROM stories`)
     res.status(200).send(results[0])
 })
+//指定文章id
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const results = await db.promise().query(`SELECT * FROM stories WHERE id = ?`, [id])
+    const post = results[0].find((post) => post.id === Number(id))
+    res.status(200).send(post)
+})
 
 //POST
 router.post('/', (req, res) => {
     const { title, content } = req.body
     if (title && content) {
         try {
-            db.promise().query(`INSERT INTO stories(title, content) VALUES('${title}', '${content}')`)
+            db.promise().query(`INSERT INTO stories(title, content) VALUES(?, ?)`, [title, content])
             res.status(201).send({msg: 'Story Created' })
         }
         catch (err) {
@@ -33,7 +40,7 @@ router.put('/', (req, res) => {
     const { id, title, content } = req.body
     if (title) {
         try {
-            db.promise().query(`UPDATE stories SET title = '${title}' WHERE id = '${id}'`)
+            db.promise().query(`UPDATE stories SET title = ? WHERE id = ?`, [title, id])
             res.status(201).send({msg: 'Title Updated' })
         }
         catch (err) {
@@ -42,7 +49,7 @@ router.put('/', (req, res) => {
     }
     else if (content) {
         try {
-            db.promise().query(`UPDATE stories SET content = '${content}' WHERE id = '${id}'`)
+            db.promise().query(`UPDATE stories SET content = ? WHERE id = ?`, [content, id])
             res.status(201).send({msg: 'Content Updated' })
         }
         catch (err) {
@@ -56,14 +63,13 @@ router.delete('/', (req, res) => {
     const { id } = req.body
     if (id) {
         try {
-            db.promise().query(`DELETE FROM stories WHERE id = '${id}'`)
+            db.promise().query(`DELETE FROM stories WHERE id = ?`, [id])
             res.status(201).send({msg: 'Story Deleted' })
         }
         catch (err) {
             console.log(err)
         }
     }
-    
 })
 
 module.exports = router
